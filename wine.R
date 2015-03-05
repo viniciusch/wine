3bdb5bc10c1a56addbfa4f99338069fe145e7162
Jose Eduardo Meireles <meireles@umn.edu>
2015-03-05 12:37:04 -0600

    initial commit

---
+++ wine.R
@@ -0,0 +1,19 @@
+wine_list = c("cabernet", "pinot", "malbec", "carmenere")
+
+choose_wine = function()
+{
+  chosen_wine = sample(wine_list, 1)
+  return(chosen_wine)
+}
+
+
+date_successful = function()
+{
+  picked_wine = choose_wine()
+	
+  if(picked_wine == "carmenere")
+    print("Yesss!")
+  else
+    print("FAIL!")
+}
+
